e91976fde9a18b949a0396fd2b1994109d33b6f0
hafizardd <hafizardi4@gmail.com>
2025-08-22 09:45:11 +0700

    tambahin query

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #%% Import Library
 import pandas as pd
 import nltk
-import pyserini
-from pyserini.analysis import Analyzer, get_lucene_analyzer
 import json
 import os
 import string
 # %%
-nltk.download('stopwords')  # Download stopwords if not already downloaded
+nltk.download('stopwords')  # Download stopwords jika belum terinstall
 # %%
 # List dari file corpus-assignment#1.txt
 docs = [
@@ -34,7 +32,10 @@ df
 df['doc_text'] = df['doc_text'].apply(lambda x: x.lower())  # Lowercase
 df['doc_text'] = df['doc_text'].apply(lambda x: ' '.join([word for word in x.split() if word not in nltk.corpus.stopwords.words('english')])) # Remove Stopword
 df['doc_text'] = df['doc_text'].apply(lambda x: ''.join([char for char in x if char not in string.punctuation])) # Remove Punctuation
-# %% Indexing - Make .jsonl file
+
+# Cek kembali df setelah preprocessing
+df
+# %% Buat file .jsonl untuk proses indexing
 output_file = 'test/index.jsonl'
 output_dir = os.path.dirname(output_file)
 
@@ -50,11 +51,22 @@ with open(output_file, 'w') as f:
         }
         json_string = json.dumps(json_record)
         f.write(json_string + '\n')
-#%%
+#%% Indexing + stemming (VsCode)
 '''
 Default Indexing dari pyserini tapi tambahin stemming, jalanin di terminal
 
 python -m pyserini.index.lucene \
+  --collection JsonCollection \
+  --input {path ke file index.jsonl} \
+  --index indexes/sample_collection_jsonl \
+  --generator DefaultLuceneDocumentGenerator \
+  --stemmer porter \
+  --threads 1 \
+  --storePositions --storeDocvectors --storeRaw
+'''
+#%% Indexing + stemming (Google Colab)
+'''
+!python -m pyserini.index.lucene \
   --collection JsonCollection \
   --input {path ke file index.jsonl} \
   --index indexes/sample_collection_jsonl \
@@ -72,13 +84,15 @@ queries = [
   'dog AND cat',
   'dog OR cat',
   'dog AND NOT cat',
-  '(bm25 OR tf-idf) AND retrieval'
+  '(bm25 OR tf-idf) AND retrieval',
+  'dog OR (cat AND mouse)',
+  'retrieval AND (neural OR bm25)'
 ]
 
+# %% Print hasil pencarian
 for query in queries:
   hits = searcher.search(query)
   print(f'\n{query}:')
 
   for i in range(len(hits)):
-    print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f}')
-# %%
+    print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f} {df[df['doc_id'] == hits[i].docid]['doc_text'].values[0]}')
