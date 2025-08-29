9daa6e4e8ae6e313562fe5ac47a39685ff6b03c9
hafizardd <hafizardi4@gmail.com>
2025-08-29 13:13:51 +0700

    feat: tugas 2 bm25 retrieval

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Boolean Retrieval
-Tugas mata kuliah Temu Kembali Informasi. Tujuan dari tugas ini adalah untuk mempelajari dasar-dasar Pengambilan Boolean dan proses membangun indeks mesin pencari sederhana menggunakan pustaka Pyserini (antarmuka Python untuk Lucene/Anserini).
+# Information Retrieval
+Tugas mata kuliah Temu Kembali Informasi. Tujuan dari tugas ini adalah untuk mempelajari dasar-dasar Pengambilan Informasai dan proses membangun indeks mesin pencari sederhana menggunakan pustaka Pyserini (antarmuka Python untuk Lucene/Anserini). Projek ini akan membandingkan bagaimana perbedaan hasil dari kueri pada metode Boolean dan BM25 secara sederhana.
 
 ## Setup
 ### 1. Clone Repository
---
+++ main-bm25.py
@@ -0,0 +1,107 @@
+#%% Import Library
+import pandas as pd
+import nltk
+import json
+import os
+import string
+# %%
+nltk.download('stopwords')  # Download stopwords jika belum terinstall
+# %%
+# List dari file corpus-assignment#1.txt
+docs = [
+  ("d1",  "The cat chased a small mouse into the garden."),
+  ("d2",  "A friendly dog played fetch by the river."),
+  ("d3",  "BM25 is a ranking function widely used in search engines."),
+  ("d4",  "Boolean retrieval uses logical operators like AND and OR."),
+  ("d5",  "TF-IDF weights terms by frequency and rarity."),
+  ("d6",  "Neural retrieval uses dense embeddings for semantic search."),
+  ("d7",  "The dog and the cat slept on the same couch."),
+  ("d8",  "The library hosts a workshop on information retrieval."),
+  ("d9",  "Students implemented BM25 and compared it with TF-IDF."),
+  ("d10", "The chef roasted chicken with rosemary and garlic."),
+  ("d11", "A black cat crossed the old stone bridge at night."),
+  ("d12", "Dogs are loyal companions during long hikes."),
+  ("d13", "The dataset contains fifteen short sentences for testing."),
+  ("d14", "Reranking models reorder BM25 candidates using transformers."),
+  ("d15", "The dog sniffed a cat but ignored the mouse.")
+]
+# %% Mengubah List Menjadi Dataframe
+df = pd.DataFrame(docs, columns=['doc_id', 'doc_text'])
+df
+# %% Preprocessing
+df['doc_text'] = df['doc_text'].apply(lambda x: x.lower())  # Lowercase
+df['doc_text'] = df['doc_text'].apply(lambda x: ' '.join([word for word in x.split() if word not in nltk.corpus.stopwords.words('english')])) # Remove Stopword
+df['doc_text'] = df['doc_text'].apply(lambda x: ''.join([char for char in x if char not in string.punctuation])) # Remove Punctuation
+
+# Cek kembali df setelah preprocessing
+df
+# %% Buat file .jsonl untuk proses indexing
+output_file = 'test/index.jsonl'
+output_dir = os.path.dirname(output_file)
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+    print(f"Created directory: {output_dir}")
+
+with open(output_file, 'w') as f:
+    for index, row in df.iterrows():
+        json_record={
+            "id": row['doc_id'],
+            "contents": row['doc_text']
+        }
+        json_string = json.dumps(json_record)
+        f.write(json_string + '\n')
+#%% Indexing + stemming (VsCode)
+'''
+Default Indexing dari pyserini tapi tambahin stemming, jalanin di terminal
+
+python -m pyserini.index.lucene \
+  --collection JsonCollection \
+  --input {path ke file index.jsonl} \
+  --index indexes/sample_collection_jsonl \
+  --generator DefaultLuceneDocumentGenerator \
+  --stemmer porter \
+  --threads 1 \
+  --storePositions --storeDocvectors --storeRaw
+'''
+#%% Indexing + stemming (Google Colab)
+'''
+!python -m pyserini.index.lucene \
+  --collection JsonCollection \
+  --input {path ke file index.jsonl} \
+  --index indexes/sample_collection_jsonl \
+  --generator DefaultLuceneDocumentGenerator \
+  --stemmer porter \
+  --threads 1 \
+  --storePositions --storeDocvectors --storeRaw
+'''
+# %% BM-25 Retrieval
+from pyserini.search.lucene import LuceneSearcher
+
+searcher = LuceneSearcher('indexes/sample_collection_jsonl')
+
+queries = [
+  'dog cat', # termasuk 'dog AND cat' dan 'dog OR cat'
+  'dog',
+  'bm25 tf-idf retrieval',
+  'dog cat mouse)',
+  'retrieval neural bm25)'
+]
+# %% Fungsi untuk print hasil pencarian
+def display_results(query, hits):
+  print(f'\n{query}:')
+
+  if not hits:
+      print("Tidak ada dokumen yang sesuai.")
+      print("-"*100)
+      return
+  
+  print('  ', 'Id', ' ', 'Score', ' ', 'Teks')
+  
+  for i in range(len(hits)):
+    print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f} {df[df['doc_id'] == hits[i].docid]['doc_text'].values[0]}')
+  print("-"*100)
+#%%
+for query in queries:
+   hits = searcher.search(query)
+   display_results(query, hits)
